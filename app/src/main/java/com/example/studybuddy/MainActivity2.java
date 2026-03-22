500bfbe130602fa270f0f06049d81d881462ab54
jeremy <uyaidan73@gmail.com>
2026-03-22 22:38:18 +0800

    studybuddycommit

---
+++ app/src/main/java/com/example/studybuddy/MainActivity2.java
@@ -0,0 +1,50 @@
+package com.example.studybuddy;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+import android.widget.Button;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.studybuddy.databinding.ActivityMain2Binding;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    private AppBarConfiguration appBarConfiguration;
+    private ActivityMain2Binding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMain2Binding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        Button loginBtn = findViewById (R.id.button2) ;
+        loginBtn.setOnClickListener (new View. OnClickListener () {
+            @Override
+            public void onClick ( View v ) {
+                // 1. Create the Intent ( From LoginActivity to DashboardActivity )
+                Intent intent = new Intent (MainActivity2.this, MainActivity3.class) ;
+
+                // 2. Start the Activity
+                startActivity (intent) ;
+            }
+        }) ;
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+        return NavigationUI.navigateUp(navController, appBarConfiguration)
+                || super.onSupportNavigateUp();
+    }
+}
