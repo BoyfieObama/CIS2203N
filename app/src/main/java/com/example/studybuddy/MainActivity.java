764f856e91f1f7d4e3489e406d2dac1749feaf92
jeremy <uyaidan73@gmail.com>
2026-03-24 01:03:00 +0800

    Part 2

--- app/src/main/java/com/example/studybuddy/MainActivity.java
+++ app/src/main/java/com/example/studybuddy/MainActivity.java
@@ -15,6 +15,9 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    int Counter = 0;
+    TextView CounterDisplay;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,20 +25,32 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar actionbar = findViewById(R.id.toolbar);
         setSupportActionBar(actionbar);
-        getSupportActionBar().setTitle("ID: 22102067");
+        getSupportActionBar().setTitle ("ID: 1234567 ") ;
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        Button theBtn = findViewById(R.id.button);
-        TextView text = findViewById(R.id.textView);
-        theBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                text.setText("Dr. Angie M. Ceniza");
-            }
+        CounterDisplay = findViewById(R.id.textView);
+        CounterDisplay.setText("0");
 
+        Button theBtn = findViewById(R.id.button);
+        theBtn.setOnClickListener(v -> {
+            Counter++;
+            CounterDisplay.setText(String.valueOf(Counter));
         });
+
+    }
+
+    @Override
+    protected void onSaveInstanceState ( Bundle outState ) {
+        super . onSaveInstanceState ( outState ) ;
+        outState . putInt (" COUNT_KEY ", Counter ) ;
+    }
+    @Override
+    protected void onRestoreInstanceState ( Bundle savedInstanceState ) {
+        super . onRestoreInstanceState ( savedInstanceState ) ;
+        Counter = savedInstanceState . getInt (" COUNT_KEY ") ;
+        CounterDisplay . setText ( String . valueOf ( Counter ) ) ;
     }
 }
